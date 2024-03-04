6180a6d3cd1d2ca9b707fe08c80df5877139f280
varshitha518 <161112238+varshitha518@users.noreply.github.com>
2024-03-04 23:02:38 +0530

    intrest

---
+++ intrest.py
@@ -0,0 +1,5 @@
+principal=int(input("Enter the principal Amount"))
+rate = float(input("rate of interst"))
+time=int(input("time period"))
+ci=(principal+rate*time)/100
+print("compound interst=",ci)
